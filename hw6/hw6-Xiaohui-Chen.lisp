a3750580ba29d71e1fb6f34572f28498f5d4369d
Xiaohui Chen <xhchen0328@utexas.edu>
2015-03-21 23:05:58 -0500

    finish hw6

--- hw6/hw6-Xiaohui-Chen.lisp
+++ hw6/hw6-Xiaohui-Chen.lisp
@@ -89,70 +89,34 @@
 	        l
 		(mem x (cdr l)))))
 
-;proof of actual theorems
+;proof of actual theorem
 
 
-(DEFTHM PROBLEM3-PART2
-        (IMPLIES (OR (MEM E A) (MEM E B))
-                 (MEM E (APP A B)))
+(DEFTHM PROBLEM3
+        (IFF (MEM E (APP A B))
+             (OR (MEM E A) (MEM E B)))
         :INSTRUCTIONS ((:INDUCT (MEM E A))
-                       (:DIVE 1 1)
-                       :EXPAND (:DV 1)
-                       :S-PROP :UP (:DV 2)
-                       :S-PROP :TOP (:DV 1 2)
-                       :EXPAND (:DV 1)
-                       :S-PROP :UP (:DV 2)
-                       :S-PROP :UP :TOP (:DV 2 2)
-                       :EXPAND :S-PROP :UP :EXPAND (:DV 1)
-                       :S-PROP :UP (:DV 2)
-                       (:DV 1 2)
-                       (:REWRITE CAR-CONS)
-                       :UP :UP :S-PROP (:DV 2)
-                       (:REWRITE CDR-CONS)
-                       :UP :UP :TOP (:DEMOTE 3)
-                       :S-PROP (:DIVE 1 1)
-                       :EXPAND (:DV 1)
-                       :S-PROP :UP (:DV 2)
-                       :S-PROP :UP :S-PROP :EXPAND (:DV 1)
-                       :S-PROP :UP (:DV 2)
-                       :S-PROP :UP :S-PROP (:DV 2)
-                       :EXPAND :S-PROP :UP :EXPAND (:DV 1)
-                       :S-PROP :UP (:DV 2)
-                       (:DV 1 2)
-                       (:REWRITE CAR-CONS)
-                       :UP :S-PROP :TOP :S-PROP (:DIVE 1 1)
+                       (:DIVE 1 2)
                        :EXPAND
-                       :S-PROP :UP :S-PROP :TOP (:DV 2 2)
+                       :S-PROP :UP :EXPAND :S-PROP (:DIVE 1 2)
+                       (:REWRITE CAR-CONS)
+                       :UP :S-PROP :UP :S-PROP (:DIVE 2)
+                       (:REWRITE CDR-CONS)
+                       :TOP (:DIVE 2 1)
+                       :EXPAND :S-PROP :UP :S :UP (:DEMOTE 3)
+                       :S-PROP (:DIVE 1 2)
+                       :EXPAND
+                       :S-PROP :UP :EXPAND :S-PROP (:DIVE 1 2)
+                       (:REWRITE CAR-CONS)
+                       :UP :S-PROP
+                       :UP :S-PROP :UP :S-PROP (:DIVE 1)
+                       :EXPAND :S-PROP :TOP :S-PROP (:DIVE 1 2)
                        :EXPAND
                        :S-PROP :UP
+                       :TOP (:DIVE 2 1)
+                       :EXPAND :S-PROP
+                       :UP :S-PROP
                        :TOP :S-PROP))
-
-(DEFTHM PROBLEM3-PART1
-        (IMPLIES (MEM E (APP A B))
-                 (OR (MEM E A) (MEM E B)))
-        :INSTRUCTIONS ((:INDUCT (MEM E A))
-                       (:DIVE 2 1)
-                       :EXPAND :S-PROP :UP (:DV 2)
-                       :EXPAND :S-PROP :UP :UP (:DV 1 2)
-                       :EXPAND
-                       :S-PROP :UP :EXPAND :S-PROP (:DV 1 2)
-                       (:REWRITE CAR-CONS)
-                       :UP :S-PROP :UP :S-PROP (:DV 2)
-                       (:REWRITE CDR-CONS)
-                       :TOP :DEMOTE :S-PROP (:DIVE 2 1)
-                       :EXPAND
-                       :S-PROP :UP :S-PROP :UP (:DV 1 2)
-                       :EXPAND :S-PROP :UP :EXPAND (:DV 2)
-                       (:DV 1 2)
-                       (:REWRITE CAR-CONS)
-                       :UP :UP :S-PROP :UP :UP :S (:DIVE 2 1)
-                       :EXPAND
-                       :S-PROP :UP :S-PROP :UP (:DV 1 2)
-                       :EXPAND
-                       :S-PROP :UP
-                       :TOP :S-PROP))
-
-
 
 
 
@@ -161,6 +125,126 @@
 
 ;function definitions
 
+;symbol-listp is redundant. Therefore I used the abbreviation slp
+
+(defun slp (lst)
+	(cond ((atom lst) (eq lst nil))
+	      (t (and (symbolp (car lst))
+		      (slp (cdr lst))))))
+
+;proof of actual theorem
+
+(DEFTHM PROBLEM4
+        (IMPLIES (SLP L)
+                 (SYMBOLP (CAR (MEM X L))))
+        :INSTRUCTIONS (:INDUCT (:DIVE 1)
+                               :EXPAND :S-PROP :TOP (:DV 2 1 1)
+                               :EXPAND
+                               :S-PROP :TOP :PROMOTE (:FORWARDCHAIN 3)
+                               :S-PROP (:DIVE 1)
+                               :EXPAND :S-PROP :TOP (:DV 2 1 1)
+                               :EXPAND :S-PROP :TOP (:DV 1 2)
+                               :TOP
+                               :S (:DV 2 1 1)
+                               :EXPAND :S-PROP
+                               :TOP :S-PROP))
+
+;-----------------Problem 5------------------
+
+;function definitions
+
+(defun xor (p q)
+    (if p 
+	(if q nil t)
+	(if q t nil)))
+
+(defun for (x y)
+    (if x t y))
+
+(defun fand (x y)
+    (if x y nil))
+
+(defun b-carry (a b c)
+    (if a 
+        (for b c)
+        (fand b c)))
+
+(defun ln (x)
+    (if (atom x)
+	nil
+	(cons nil (ln (cdr x)))))
+
+(defun blp (x)
+    (if (atom x)
+	(null x)
+	(and (or (equal (car x) t)
+		 (equal (car x) nil))
+	     (blp (cdr x)))))
+
+(defun v-add (c a b)
+    (if (atom a)
+	(list c)
+	(cons (xor c (xor (car a) (car b)))
+	      (v-add (b-carry c (car a) (car b))
+		     (cdr a)
+		     (cdr b)))))
+
+;proof of actual theorem
+
+(DEFTHM PROBLEM5-LEMMA1
+        (IMPLIES (AND (BLP A)
+                      (BLP B)
+                      (EQUAL (LN A) (LN B)))
+                 (EQUAL (B-CARRY C (CAR A) (CAR B))
+                        (B-CARRY C (CAR B) (CAR A))))
+        :INSTRUCTIONS ((:DV 1 1)
+                       :EXPAND :UP (:DV 2)
+                       :EXPAND :TOP :PROMOTE (:DEMOTE 1)
+                       (:DEMOTE 1)
+                       :SPLIT (:DV 1)
+                       :EXPAND :S-PROP (:DV 2)
+                       :EXPAND :TOP (:DV 2)
+                       :EXPAND
+                       :S-PROP :TOP
+                       :S (:DV 1)
+                       :EXPAND :S-PROP
+                       :TOP (:DV 2)
+                       :EXPAND :S-PROP
+                       :TOP :S
+                       :S :S))
+
+(DEFTHM PROBLEM5
+        (IMPLIES (AND (BLP A)
+                      (BLP B)
+                      (EQUAL (LN A) (LN B)))
+                 (EQUAL (V-ADD C A B) (V-ADD C B A)))
+        :INSTRUCTIONS ((:INDUCT (V-ADD C A B))
+                       :PROMOTE (:FORWARDCHAIN 2)
+                       (:DV 1)
+                       :EXPAND :S-PROP :TOP (:DV 2)
+                       :EXPAND
+                       :TOP :SPLIT (:REWRITE CONS-EQUAL)
+                       (:DV 1)
+                       :S :S :TOP :S-PROP (:DV 2 1)
+                       (:REWRITE PROBLEM5-LEMMA1)
+                       :TOP :S-PROP (:DEMOTE 4)
+                       (:DV 1 1)
+                       :EXPAND :S-PROP :UP (:DV 2)
+                       :EXPAND :S-PROP
+                       :UP :S-PROP :TOP :S-PROP (:DV 1 1)
+                       :EXPAND :S-PROP :UP (:DV 2)
+                       :EXPAND :S-PROP :TOP :SPLIT (:DEMOTE 4)
+                       (:DV 1 1)
+                       :EXPAND :S-PROP :UP (:DV 2)
+                       :EXPAND :S-PROP
+                       :UP :S-PROP :TOP :S-PROP (:DEMOTE 4)
+                       (:DV 1)
+                       (:DV 1)
+                       :EXPAND :S-PROP
+                       :UP (:DV 2)
+                       :EXPAND :S-PROP
+                       :UP :S-PROP
+                       :TOP :S-PROP))
 
 
 
