82bc52c413b0ede569371cb60fc95c6ed240f93d
Xiaohui Chen <xhchen0328@utexas.edu>
2015-04-04 18:48:20 -0500

    fix state bug

--- project/code/model.lisp
+++ project/code/model.lisp
@@ -11,37 +11,39 @@ For each state, the model is with the form
 		    ...))
 ||#
 
+(local (include-book "arithmetic/top-with-meta" :dir :system))
+
 ;definitions for the j-state
 
 (defun rational-pair (x)
 	(if (atom x)
 	    nil
 	    (and (rationalp (car x))
+		 (<= 0 (car x))
 		 (rationalp (cdr x))
 		 (>= (car x) (abs (cdr x))))))
 
 (defun true-jstate (a)
 	(if (atom a)
-	    (if (= a 0)
-		t
-		nil)
+	    (equal a 0)
 	    (and (rationalp (car a))
+		 (< 0 (car a))
 	         (rational-pair (cdr a)))))
-#||
-(defun j-state (a j m_j)
-      (declare (xargs :guard (and (rationalp a)
-				  (rationalp j)
-				  (rationalp m_j))))
-      (cons a (cons j m_j)))
-||#
 
 (defun j-lowering (x)
-	(if (< (car (cdr x)) (abs (- (cdr (cdr x)) 1)))
+	(if (<= (car (cdr x)) (- (cdr (cdr x))))
 	    0
 	    (cons (* (car x)
 		     (- (*  (car (cdr x)) (+ (car (cdr x)) 1))
 		        (* (cdr (cdr x)) (- (cdr (cdr x)) 1))))
 		  (cons (car (cdr x)) (- (cdr (cdr x)) 1)))))
+
+(defthm j-lowering-valid-lemma1 
+	(implies (true-jstate x)
+		 (xor (equal (j-lowering x) 0)
+		     (< 0 (- (*  (car (cdr x)) (+ (car (cdr x)) 1))
+		        (* (cdr (cdr x)) (- (cdr (cdr x)) 1)))))))
+		 
 
 (defthm j-lowering-valid
 	(implies (true-jstate x)
@@ -108,14 +110,28 @@ For each state, the model is with the form
 
 ;clean up 0s in the list
 
+(defun all-zeros (x)
+	(if (atom x)
+	    t
+	    (and (equal (car x) 0)
+		 (all-zeros (cdr x)))))
+
+(defun clean-up-zero-list (x) 
+	(if (atom x)
+	    0
+            (if (equal (car x) 0)
+		(clean-up-zero-list (cdr x))
+	        (cons (car x)
+		      (clean-up-zero-list (cdr x))))))
+
 (defun clean-up-zero (x)
 	(if (atom x)
 	    0
-	    (if (equal (car x) 0)
-		(clean-up-zero (cdr x))
-	        (cons (car x)
-		      (clean-up-zero (cdr x))))))
+	    (if (all-zeros x)
+		0
+	        (clean-up-zero-list x))))
 
+ 
 (defthm clean-up-zero-s-lowering-valid
 	(implies (good-quantumcoupled x)
 		 (good-quantumcoupled (clean-up-zero 
@@ -126,10 +142,41 @@ For each state, the model is with the form
 		 (good-quantumcoupled (clean-up-zero 
 					   (l-lowering x)))))
 
-;TODO: merge the same state
+;merge the same state
 
+(defun coupled-coefficient (x)
+	(car x))
+
+(defun coupled-state (x)
+	(cdr x))
+
+(defun add-same-state (x y)
+	(if (atom y)
+	    x
+	    (if (equal (coupled-state x)
+		       (coupled-state (car y)))
+	        (add-same-state (cons (+ (coupled-coefficient x)
+					 (coupled-coefficient 
+							(car y)))
+			      	      (coupled-state x))
+			 	(cdr y))
+		(add-same-state x (cdr y)))))
+
+(defun delete-same-state (x y)
+	(if (atom y)
+	    y
+	    (if (equal (coupled-state x)
+		       (coupled-state (car y)))
+		(delete-same-state x (cdr y))
+		(cons (car y) (delete-same-state x (cdr y))))))
+
+(defun merge-same (x)
+	(if (atom x)
+	    x
+	    (cons (add-same-state (car x) (cdr x))
+	 	  (merge-same 
+			(delete-same-state (car x) (cdr x))))))
 	    
-
 ;verify l-lowering-valid and s-lowering-valid
 
 (defthm l-lowering-valid 
@@ -162,22 +209,159 @@ For each state, the model is with the form
 	(+ (abs (car (car x)))
 	   (sum-of-coefficients (cdr x)))))
 
+(defun get-jcoefficient (x)
+	(if (atom x)
+	    0
+	    (car x)))
+
 (defun true-state (x)
     (if (atom x)
 	nil
 	(and (true-jstate (car x))
 	     (good-quantumcoupled (cdr x))
+	     (iff (consp (car x)) (consp (cdr x)))
+	     (iff (equal (car x) 0) (equal (cdr x) 0))
   	     (= (sum-of-coefficients (cdr x))
-		(abs (car (car x)))))))
+		(get-jcoefficient (car x))))))
 
-(defun state-lowering (x)
+;before applying the operators, we have to normalize the states
+(defun j-coefficient (x)
+	 (car (car x)))
+
+(defun j-state (x)
+	(cdr (car x)))
+
+(defun normalize-j (a)
+	(if (atom a)
+	    0
+	    (cons (/ (car a) (car a)) (cdr a))))
+
+(defun divide-coefficient (x y)
+	(cons (/ (car y) x)
+	      (cdr y)))
+
+(defun normalize-coupled-helper (x y)
+	(if (atom y)
+	    nil
+	    (cons (divide-coefficient x (car y))
+		  (normalize-coupled-helper x (cdr y)))))
+
+(defun normalize-coupled (x y)
+	(if (atom y)
+	    0
+	    (normalize-coupled-helper x y)))
+
+(defun normalize-state (x)
+	(if (equal (j-coefficient x) 0)
+	    x
+	    (cons (normalize-j (car x))
+	          (normalize-coupled (j-coefficient x) (cdr x)))))
+
+(defthm normalize-coupled-valid-trivial
+	(implies (and (atom x)
+		      (rationalp a)
+		      (good-quantumcoupled x))
+		 (atom (normalize-coupled a x))))
+
+(defthm normalize-coupled-valid-non-trival
+	(implies (and (good-quantumcoupled x)
+		      (rationalp a)
+		      (consp x))
+		 (good-quantumcoupled 
+			(normalize-coupled-helper a x))))
+
+(defthm normalize-coupled-valid 
+	(implies (and (good-quantumcoupled x)
+		      (rationalp a))
+		 (good-quantumcoupled 
+		      (normalize-coupled a x))))
+
+(defthm normalize-j-valid 
+	(implies (true-jstate x)
+		 (true-jstate (normalize-j x))))
+
+(defthm true-state-trival
+	(implies (true-state x)
+		 (xor (and (equal (car x) 0)
+			  (equal (cdr x) 0))
+		     (and (consp (car x))
+			  (consp (cdr x))))))
+
+(defthm normalize-state-car
+	(implies (true-state x)
+		 (true-jstate (car (normalize-state x)))))
+
+(defthm normalize-state-cdr 
+	(implies (true-state x)
+		 (good-quantumcoupled (cdr (normalize-state x)))))
+
+#||
+(defthm sum-of-coefficient-div-multiply
+	(implies (and (good-quantumcoupled x)
+		      (rationalp a)
+		      (rationalp b)
+		      (> a 0)
+		      (> b 0)
+		      (equal (sum-of-coefficients x) b))
+		 (equal (/ b a) (sum-of-coefficients
+				   (normalize-coupled a x)))))
+		      
+(defthm sum-of-coefficient-valid 
+	(implies  (true-state x)
+		(= (sum-of-coefficients (cdr (normalize-state x)))
+		   (get-jcoefficient (car (normalize-state x))))))
+
+
+(DEFTHM NORMALIZE-STATE-VALID-LEMMA1
+        (IMPLIES (AND (CONSP X)
+                      (CONSP (CAR X))
+                      (RATIONALP (CAAR X))
+                      (CONSP (CDAR X))
+                      (RATIONALP (CADAR X))
+                      (RATIONALP (CDDAR X))
+                      (< (CDDAR X) 0)
+                      (<= (- (CDDAR X)) (CADAR X))
+                      (GOOD-QUANTUM-COUPLED-LIST (CDR X))
+                      (CONSP (CDR X))
+                      (EQUAL (SUM-OF-COEFFICIENTS (CDR X))
+                             (CAAR X)))
+                 (CONSP (NORMALIZE-COUPLED-HELPER (CAAR X)
+                                                  (CDR X))))
+        :INSTRUCTIONS (:PROMOTE (:DV 1)
+                                :EXPAND :S-PROP
+                                :TOP :S))
+
+		      
+
+||#
+
+(defthm sum-multiply-div 
+	(implies (and (good-quantumcoupled x)
+		      (rationalp a)
+		      (< 0 a))
+		 (equal (sum-of-coefficients 
+				(normalize-coupled a x))
+			(/ (sum-of-coefficients x) a)))) 
+
+
+(defthm normalize-state-valid
+	(implies (true-state x)
+		 (true-state (normalize-state x))))
+
+
+;applying the lowering operators
+(defun state-lowering-helper (x)
 	(if (atom x)
 	    0
 	    (cons (j-lowering (car x))
-		  (state-append (clean-up-zero 
+		  (merge-same (state-append (clean-up-zero 
 				   (l-lowering (cdr x)))
 			         (clean-up-zero
-				   (s-lowering (cdr x)))))))
+				   (s-lowering (cdr x))))))))
+
+(defun state-lowering (x)
+	(state-lowering-helper (normalize-state x)))
+
 
 #||
 (defthm sum-equal 
